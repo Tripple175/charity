18308eacafab1bc6d0bc089817db1af51b55eefc
Tripple175 <hptripple@gmail.com>
2022-09-29 19:01:59 +0300

    my commit

--- charity/settings.py
+++ charity/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-p^otpqp-ch3$z&fz+9!mcb%7yv+g@2s99t&c2%i9uh*efj44&s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
